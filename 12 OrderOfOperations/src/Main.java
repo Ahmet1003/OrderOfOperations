a1f3351abcd6f78618dfcbc355cd4c212c647a1b
Ahmet1003 <34312116+Ahmet1003@users.noreply.github.com>
2024-10-31 00:40:36 +0300

    Add files via upload

---
+++ 12 OrderOfOperations/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x = 10 + 2 * 3;
+        System.out.println(x);
+
+        int y = (10 + 2) * 3;
+        System.out.println(y);
+
+        // So the order of operations are as follows;
+
+        // 1. ()
+        // 2. *  /
+        // 3. +  -
+    }
+}
